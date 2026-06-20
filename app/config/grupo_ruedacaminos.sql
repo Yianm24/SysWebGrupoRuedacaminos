4b131884fe76d46d9640300decb0278957f17bee
HoneyMilu <malau200104@gmail.com>
2026-06-20 18:20:10 -0400

    base de datos actualizada con cambios en la primary key de empleado

--- app/config/grupo_ruedacaminos.sql
+++ app/config/grupo_ruedacaminos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-06-2026 a las 03:53:52
+-- Tiempo de generación: 21-06-2026 a las 00:18:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -33,6 +33,15 @@ CREATE TABLE `banco` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `banco`
+--
+
+INSERT INTO `banco` (`cod_banco`, `nombre`, `estado`) VALUES
+(1, 'BNC', 1),
+(2, 'Banplus', 1),
+(3, 'Banesco', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +55,18 @@ CREATE TABLE `cambio_moneda` (
   `cod_moneda` int(11) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cambio_moneda`
+--
+
+INSERT INTO `cambio_moneda` (`cod_cambio`, `tasa`, `fecha`, `cod_moneda`, `estado`) VALUES
+(1, 602.25, '2026-06-18', 1, 1),
+(2, 645.03, '2026-06-18', 3, 1),
+(3, 805.12, '2026-06-18', 4, 1),
+(4, 602.25, '2026-06-18', 1, 1),
+(5, 645.03, '2026-06-18', 3, 1),
+(6, 805.12, '2026-06-18', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -66,7 +87,8 @@ CREATE TABLE `cargo` (
 INSERT INTO `cargo` (`cod_cargo`, `nombre`, `estado`) VALUES
 (1, 'administracion', 1),
 (2, 'contaduria', 1),
-(3, 'presidencia', 1);
+(3, 'presidencia', 1),
+(4, 'chofer', 1);
 
 -- --------------------------------------------------------
 
@@ -80,6 +102,16 @@ CREATE TABLE `ciudad` (
   `cod_municipio` int(2) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ciudad`
+--
+
+INSERT INTO `ciudad` (`cod_ciudad`, `nombre`, `cod_municipio`, `estado`) VALUES
+(1, 'Barquisimeto', 1, 1),
+(2, 'Valencia', 3, 1),
+(3, 'Maracaibo', 4, 1),
+(4, 'Cabudare', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -122,6 +154,15 @@ CREATE TABLE `cuenta_banco` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `cuenta_banco`
+--
+
+INSERT INTO `cuenta_banco` (`cod_cuenta`, `telefono`, `numero_cuenta`, `cod_banco`, `estado`) VALUES
+(1, '0412394949', '192912912112919', 3, 1),
+(2, '04123838383', '011202010201444332', 2, 1),
+(3, '04123838383', '01032020102033248', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -130,7 +171,7 @@ CREATE TABLE `cuenta_banco` (
 
 CREATE TABLE `despacho` (
   `cod_despacho` int(11) NOT NULL,
-  `cod_empleado` varchar(8) NOT NULL,
+  `cod_empleado` int(2) NOT NULL,
   `cod_vehiculo` int(2) NOT NULL,
   `cod_detalledespacho` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -147,6 +188,15 @@ CREATE TABLE `detalle_despacho` (
   `estatus` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `detalle_despacho`
+--
+
+INSERT INTO `detalle_despacho` (`cod_detalledespacho`, `fecha`, `estatus`) VALUES
+(1, '2026-06-19 00:20:29', 'Cargando'),
+(2, '2026-06-19 00:21:09', 'Despachand'),
+(3, '2026-06-19 00:21:49', 'Entregado');
+
 -- --------------------------------------------------------
 
 --
@@ -161,6 +211,14 @@ CREATE TABLE `detalle_pago` (
   `monto` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `detalle_pago`
+--
+
+INSERT INTO `detalle_pago` (`cod_detallepago`, `referencia`, `cod_metodopago`, `cod_banco`, `monto`) VALUES
+(1, 30232, 1, 3, 10000),
+(2, 772791, 2, 1, 13892.5);
+
 -- --------------------------------------------------------
 
 --
@@ -168,12 +226,13 @@ CREATE TABLE `detalle_pago` (
 --
 
 CREATE TABLE `empleado` (
-  `cod_empleado` varchar(8) NOT NULL,
+  `cod_empleado` int(2) NOT NULL,
+  `cedula` varchar(8) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `telefono` varchar(12) NOT NULL,
   `telefono_emergencia` varchar(12) DEFAULT NULL,
-  `cod_cargo` int(1) DEFAULT NULL,
+  `cod_cargo` int(1) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -181,9 +240,10 @@ CREATE TABLE `empleado` (
 -- Volcado de datos para la tabla `empleado`
 --
 
-INSERT INTO `empleado` (`cod_empleado`, `nombre`, `apellido`, `telefono`, `telefono_emergencia`, `cod_cargo`, `estado`) VALUES
-('2525', 'juan', 'jochis', '024255003', '565656', 2, 1),
-('8888', 'mari', 'pere', '024255003', '565656', 1, 1);
+INSERT INTO `empleado` (`cod_empleado`, `cedula`, `nombre`, `apellido`, `telefono`, `telefono_emergencia`, `cod_cargo`, `estado`) VALUES
+(2525, '', 'juan', 'jochis', '024255003', '565656', 2, 1),
+(8888, '', 'mari', 'pere', '024255003', '565656', 1, 1),
+(15151, '', 'antonis', 'caraqueña', '040656060', '87878481', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -217,6 +277,15 @@ CREATE TABLE `estado` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`cod_estado`, `nombre`, `estado`) VALUES
+(1, 'Lara', 1),
+(2, 'Carabobo', 1),
+(3, 'Zulia', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -227,6 +296,15 @@ CREATE TABLE `gastos_funcionales` (
   `cod_gasto` int(11) NOT NULL,
   `detalles` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `gastos_funcionales`
+--
+
+INSERT INTO `gastos_funcionales` (`cod_gasto`, `detalles`) VALUES
+(1, 'Viaticos'),
+(2, 'Peajes'),
+(3, 'Gasolina');
 
 -- --------------------------------------------------------
 
@@ -240,6 +318,15 @@ CREATE TABLE `marca` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`cod_marca`, `nombre`, `estado`) VALUES
+(1, 'FORD', 1),
+(2, 'FIAT', 1),
+(3, 'CHEVROLET', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -252,6 +339,17 @@ CREATE TABLE `metodo_pago` (
   `cod_moneda` int(11) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `metodo_pago`
+--
+
+INSERT INTO `metodo_pago` (`cod_metodo`, `nombre`, `cod_moneda`, `estado`) VALUES
+(1, 'Pago Movil', 2, 1),
+(2, 'Transferencia', 2, 1),
+(3, 'Efectivo', 1, 1),
+(4, 'Zelle', 1, 1),
+(5, 'PayPal', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -288,6 +386,16 @@ CREATE TABLE `moneda` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `moneda`
+--
+
+INSERT INTO `moneda` (`cod_moneda`, `nombre`, `abreviatura`, `estado`) VALUES
+(1, 'Dolar', 'USD', 1),
+(2, 'Bolívar ', 'VES', 1),
+(3, 'Euro', 'EUR', 1),
+(4, 'Tether', 'USDT', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -300,6 +408,16 @@ CREATE TABLE `municipio` (
   `cod_estado` int(2) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `municipio`
+--
+
+INSERT INTO `municipio` (`cod_municipio`, `nombre`, `cod_estado`, `estado`) VALUES
+(1, 'Irribaren', 1, 1),
+(2, 'Palavecino', 1, 1),
+(3, 'Valencia', 2, 1),
+(4, 'Maracaibo', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -326,10 +444,24 @@ CREATE TABLE `pago` (
 
 CREATE TABLE `parroquia` (
   `cod_parroquia` int(2) NOT NULL,
-  `nombre` int(15) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
   `cod_ciudad` int(2) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `parroquia`
+--
+
+INSERT INTO `parroquia` (`cod_parroquia`, `nombre`, `cod_ciudad`, `estado`) VALUES
+(1, 'Guerrera Ana Soto', 1, 1),
+(2, 'Concepción', 1, 1),
+(3, 'Catedral', 1, 1),
+(4, 'Cabudare', 4, 1),
+(5, 'Agua Viva', 4, 1),
+(6, 'Candelaria', 2, 1),
+(7, 'Antonio Borjas Romero', 3, 1),
+(8, 'Bolívar', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -363,8 +495,7 @@ CREATE TABLE `precio_kilometraje` (
 INSERT INTO `precio_kilometraje` (`cod_preciokilometraje`, `kilometraje`, `monto_tarifa`, `estado`) VALUES
 (1, '1km', 2.35, 1),
 (2, '5km', 4.50, 1),
-(3, '10km', 9.00, 1),
-(4, '6km', 777.00, 0);
+(3, '10km', 9.00, 1);
 
 -- --------------------------------------------------------
 
@@ -377,6 +508,14 @@ CREATE TABLE `rol` (
   `nombre` varchar(10) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`cod_rol`, `nombre`, `estado`) VALUES
+(1, 'Administra', 1),
+(2, 'Recepcioni', 1);
 
 -- --------------------------------------------------------
 
@@ -399,9 +538,9 @@ CREATE TABLE `tipos_vehiculo` (
 --
 
 INSERT INTO `tipos_vehiculo` (`cod_tipovehiculo`, `nombre`, `altura_max`, `peso_max`, `largo_max`, `anchura_max`, `estado`) VALUES
-(1, 'Grande', NULL, NULL, NULL, NULL, 1),
-(2, 'Mediano', NULL, NULL, NULL, NULL, 1),
-(3, 'Pequeño', NULL, NULL, NULL, NULL, 1);
+(1, 'Grande', 200, 3000, 400, 200, 1),
+(2, 'Mediano', 170, 2400, 300, 150, 1),
+(3, 'Pequeño', 150, 1800, 220, 130, 1);
 
 -- --------------------------------------------------------
 
@@ -415,6 +554,14 @@ CREATE TABLE `ubicacion` (
   `cod_parroquia` int(2) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ubicacion`
+--
+
+INSERT INTO `ubicacion` (`cod_ubicacion`, `descripcion`, `cod_parroquia`, `estado`) VALUES
+(1, 'Av. Florencio Jiménez con calle 10 de Pueblo Nuevo', 1, 1),
+(2, 'Av. Venezuela con calles 28 y 29', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -441,6 +588,15 @@ CREATE TABLE `unidades_medida` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `unidades_medida`
+--
+
+INSERT INTO `unidades_medida` (`cod_unidad`, `nombre`, `abreviatura`, `estado`) VALUES
+(1, 'Centimetros', 'CM', 1),
+(2, 'Kilometros', 'KM', 1),
+(3, 'Kilogramos', 'KG', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -455,6 +611,14 @@ CREATE TABLE `usuario` (
   `cod_rol` int(1) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`cod_usuario`, `nombre`, `password`, `telefono`, `cod_rol`, `estado`) VALUES
+('1', 'Administrador', '123456', '04123838383', 1, 1),
+('2', 'Recepcionista', '123456', '04243949940', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -693,25 +857,25 @@ ALTER TABLE `vehiculo`
 -- AUTO_INCREMENT de la tabla `banco`
 --
 ALTER TABLE `banco`
-  MODIFY `cod_banco` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_banco` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `cambio_moneda`
 --
 ALTER TABLE `cambio_moneda`
-  MODIFY `cod_cambio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_cambio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `cargo`
 --
 ALTER TABLE `cargo`
-  MODIFY `cod_cargo` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cod_cargo` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `ciudad`
 --
 ALTER TABLE `ciudad`
-  MODIFY `cod_ciudad` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_ciudad` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
@@ -723,49 +887,49 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `cuenta_banco`
 --
 ALTER TABLE `cuenta_banco`
-  MODIFY `cod_cuenta` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_cuenta` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_despacho`
 --
 ALTER TABLE `detalle_despacho`
-  MODIFY `cod_detalledespacho` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_detalledespacho` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_pago`
 --
 ALTER TABLE `detalle_pago`
-  MODIFY `cod_detallepago` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_detallepago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `envio`
 --
 ALTER TABLE `envio`
-  MODIFY `cod_envio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_envio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `estado`
 --
 ALTER TABLE `estado`
-  MODIFY `cod_estado` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_estado` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `gastos_funcionales`
 --
 ALTER TABLE `gastos_funcionales`
-  MODIFY `cod_gasto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_gasto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `cod_marca` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_marca` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `metodo_pago`
 --
 ALTER TABLE `metodo_pago`
-  MODIFY `cod_metodo` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_metodo` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `modelo`
@@ -777,25 +941,25 @@ ALTER TABLE `modelo`
 -- AUTO_INCREMENT de la tabla `moneda`
 --
 ALTER TABLE `moneda`
-  MODIFY `cod_moneda` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_moneda` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `municipio`
 --
 ALTER TABLE `municipio`
-  MODIFY `cod_municipio` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_municipio` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `pago`
 --
 ALTER TABLE `pago`
-  MODIFY `cod_pago` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `parroquia`
 --
 ALTER TABLE `parroquia`
-  MODIFY `cod_parroquia` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_parroquia` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `precio_kilometraje`
@@ -807,19 +971,19 @@ ALTER TABLE `precio_kilometraje`
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `cod_rol` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_rol` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `ubicacion`
 --
 ALTER TABLE `ubicacion`
-  MODIFY `cod_ubicacion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_ubicacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `unidades_medida`
 --
 ALTER TABLE `unidades_medida`
-  MODIFY `cod_unidad` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `cod_unidad` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculo`
@@ -853,9 +1017,9 @@ ALTER TABLE `cuenta_banco`
 -- Filtros para la tabla `despacho`
 --
 ALTER TABLE `despacho`
-  ADD CONSTRAINT `despacho_ibfk_1` FOREIGN KEY (`cod_empleado`) REFERENCES `empleado` (`cod_empleado`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `despacho_ibfk_2` FOREIGN KEY (`cod_vehiculo`) REFERENCES `vehiculo` (`cod_vehiculo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `despacho_ibfk_3` FOREIGN KEY (`cod_detalledespacho`) REFERENCES `detalle_despacho` (`cod_detalledespacho`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `despacho_ibfk_3` FOREIGN KEY (`cod_detalledespacho`) REFERENCES `detalle_despacho` (`cod_detalledespacho`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `despacho_ibfk_4` FOREIGN KEY (`cod_empleado`) REFERENCES `empleado` (`cod_empleado`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `detalle_pago`
