6b2dbbd1f454489a19d6d3f89cfa5f4d1b26ec74
Jose <jfabregamogollon@gmail.com>
2026-06-21 19:16:20 -0400

    Ajuste Base de Datos

--- app/config/grupo_ruedacaminos.sql
+++ app/config/grupo_ruedacaminos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-06-2026 a las 00:18:02
+-- Tiempo de generación: 22-06-2026 a las 01:10:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
