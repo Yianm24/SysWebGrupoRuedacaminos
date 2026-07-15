cf76244c1d78146e6ec0f90da5577a7133efd239
Jose <jfabregamogollon@gmail.com>
2026-07-15 10:44:18 -0400

    Avance en Modelo Empleado, registro, obtener cargo, y modificar

--- app/model/Empleado.php
+++ app/model/Empleado.php
@@ -47,10 +47,15 @@ class Empleado extends Conexion
         $count->execute();
         return $count->fetchColumn() > 0;
     }
-    public function regDatosEmpleado($nombre)
+    public function regDatosEmpleado($nombre, $apellido, $cedula, $telefono, $telefono_emergencia, $cod_cargo)
     {
 
         $this->nombre = $this->formatearPalabra($nombre);
+        $this->apellido = $this->formatearPalabra($apellido);
+        $this->cedula = $cedula;
+        $this->telefono = $telefono;
+        $this->telefono_emergencia = $telefono_emergencia;
+        $this->cod_cargo = $cod_cargo;
         $this->estado = 1;
 
         return $this->registrarEmpleado();
@@ -60,12 +65,17 @@ class Empleado extends Conexion
     private function registrarEmpleado()
     {
         try {
-            $sentencia = "INSERT INTO empleado (nombre, estado) VALUES (?, ?)";
+            $sentencia = "INSERT INTO empleado (nombre, apellido, cedula, telefono, telefono_emergencia, cod_cargo, estado) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
             $insert = $this->conexion->prepare($sentencia);
 
             $insert->bindValue(1, $this->nombre);
-            $insert->bindValue(2, $this->estado);
+            $insert->bindValue(2, $this->apellido);
+            $insert->bindValue(3, $this->cedula);
+            $insert->bindValue(4, $this->telefono);
+            $insert->bindValue(5, $this->telefono_emergencia);
+            $insert->bindValue(6, $this->cod_cargo);
+            $insert->bindValue(7, $this->estado);
 
             $resultado = $insert->execute();
 
@@ -87,11 +97,16 @@ class Empleado extends Conexion
     private function modificarEmpleado()
     {
         try {
-            $sentencia = "UPDATE `empleado` SET nombre = ? WHERE cod_empleado = ?";
+            $sentencia = "UPDATE `empleado` SET cedula = ?, nombre = ?, apellido = ?, telefono = ?, telefono_emergencia = ?, cod_cargo = ? WHERE cod_empleado = ?";
             $update = $this->conexion->prepare($sentencia);
 
-            $update->bindValue(1, $this->nombre);
-            $update->bindValue(2, $this->cod_empleado);
+            $update->bindValue(1, $this->cedula);
+            $update->bindValue(2, $this->nombre);
+            $update->bindValue(3, $this->apellido);
+            $update->bindValue(4, $this->telefono);
+            $update->bindValue(5, $this->telefono_emergencia);
+            $update->bindValue(6, $this->cod_cargo);
+            $update->bindValue(7, $this->cod_empleado);
 
             $update->execute();
         } catch (\PDOException $e) {
